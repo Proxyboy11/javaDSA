aa347e61656cceae7ce787bcd5246b132e26a343
TechSavvyIshan <164377945+Proxyboy11@users.noreply.github.com>
2024-09-16 11:49:37 +0530

    Add files via upload

---
+++ src/maze.java
@@ -0,0 +1,15 @@
+public class maze {
+    public static void main(String[] args) {
+        System.out.println(function(3,3));
+    }
+    static int function(int row , int cols ){
+        // base condition for last row or last column , only one way
+        if(row == 1 || cols == 1){
+            return 1 ;
+        }
+        // the total no of ways possible are no of answers from left recursion calls and right calls
+        int left = function(row-1 , cols );
+        int right = function(row , cols);
+        return left + right ;
+    }
+}
